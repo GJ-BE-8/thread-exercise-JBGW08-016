5d5ac3aa67d90fb016caa5ce2a1776ac151058b7
gimzaezin <gimzaezin@gmail.com>
2024-09-10 16:45:08 +0900

    TODO

--- src/main/java/com/nhnacademy/App.java
+++ src/main/java/com/nhnacademy/App.java
@@ -45,7 +45,7 @@ public class App
         log.debug("threadB-state:{}",threadB.getState());
 
         //TODO#1 Main Thread가 threadA, ThreadB가 종료될 때 까지 대기 합니다. Thread.yield를 사용 합니다.
-        while( !(threadA.isAlive() && threadB.isAlive()) ) {
+        while( threadA.isAlive() || threadB.isAlive() ) {
             Thread.yield();
         }
 
