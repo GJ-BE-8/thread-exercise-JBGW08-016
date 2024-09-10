03fe9172b12018656f545c5a40e079d2b7ae554b
gimzaezin <gimzaezin@gmail.com>
2024-09-10 14:30:45 +0900

    TODO

--- src/main/java/com/nhnacademy/thread/CounterIncreaseHandler.java
+++ src/main/java/com/nhnacademy/thread/CounterIncreaseHandler.java
@@ -12,6 +12,8 @@
 
 package com.nhnacademy.thread;
 
+import static java.lang.Thread.currentThread;
+
 import com.nhnacademy.count.SharedCounter;
 import lombok.extern.slf4j.Slf4j;
 
@@ -23,24 +25,27 @@ public class CounterIncreaseHandler implements Runnable {
 
     public CounterIncreaseHandler(SharedCounter sharedCounter) {
         //TODO#2-1 sharedCounter를 초기화 합니다.  sharedCounter가 null 이면 IllegalArgumentException이 발생 합니다.
-        this.sharedCounter = null;
+        if(sharedCounter == null) {
+            throw  new IllegalArgumentException();
+        }
+        this.sharedCounter = sharedCounter;
     }
 
     @Override
     public void run() {
         //TODO#2-2 현제 Thread의 interrupted이 ture <--  while의 종료조건 : interrupt가 발생 했다면 종료 합니다.
-        while(true/* whlie 조건을 수정 하세요!*/) {
+        while(!currentThread().isInterrupted()) {
             try {
                 Thread.sleep(1000);
                 //TODO 2-3 sharedCounter의 count를 1증가 시키고 count값을 반환 합니다.
-                long count = 0l;
+                long count = sharedCounter.increaseAndGet();
 
-                log.debug("thread:{}, count:{}", Thread.currentThread().getName(), count);
+                log.debug("thread:{}, count:{}", currentThread().getName(), count);
             } catch (Exception e) {
-                log.debug("{} - interrupt!",Thread.currentThread().getName());
+                log.debug("{} - interrupt!", currentThread().getName());
 
                 //TODO#2-4 현제 Thread에 interrupt()를 호출하여 interrput()를 발생 시킵 니다. 즉 현제 Thread의 interrupted 값이 -> true로 변경 됩니다. -> 즉 while 문을 종료하게 됩니다.
-
+                currentThread().interrupt();
             }
         }
     }
